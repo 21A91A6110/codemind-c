c97edd7a3bce238efd913e533310a53a4acdf5e2
21A91A6110 <96579543+21A91A6110@users.noreply.github.com>
2022-05-19 09:57:52 +0530

    #include<stdio.h> #include<math.h> #define PI 3.14 int main() { float radius,area; scanf("%f",&radius); area=PI*radius*radius; printf("%0.2f ",area); return 0; }

--- Area_of_a_circle.c
+++ Area_of_a_circle.c
@@ -3,10 +3,10 @@
 #define PI 3.14
 int main()
 {
-  float radius,area;
-  scanf("%f",&radius);
-  area=PI*radius*radius;
-  printf("%0.2f
+float radius,area;
+scanf("%f",&radius);
+area=PI*radius*radius;
+printf("%0.2f
 ",area);
-  return 0;
+return 0;
 }
